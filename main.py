be0d2342f4fa64ecc9d5973d7854f15aebdd3d6b
mathematicalninja <github@mathematics.ninja>
2025-12-14 14:26:45 +0000

    autocommit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 
-def game_loop(screen: pygame.Surface):
+def game_loop(screen: pygame.Surface, dt: float, clock: pygame.time.Clock):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +13,9 @@ def game_loop(screen: pygame.Surface):
         screen.fill("black")
         pygame.display.flip()
         pass
+
+        ticked = clock.tick(60)
+        dt = ticked / 1000
 
 
 def main():
@@ -26,8 +29,15 @@ def main():
     # Initialising pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    game_loop(screen)
+    # calling the game loop, so it's separated.
+    game_loop(
+        screen=screen,  #
+        dt=dt,
+        clock=clock,
+    )
 
 
 if __name__ == "__main__":
